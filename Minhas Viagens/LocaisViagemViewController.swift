08f0837dc6b654b343607390fe93b34813a55c9f
Thalyta Souza <mayerflot@gmail.com>
2023-12-22 15:04:27 -0300

    first commit

---
+++ Minhas Viagens/LocaisViagemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocaisViagemViewController.swift
+//  Minhas Viagens
+//
+//  Created by Thalytinha on 11/12/23.
+//
+
+import UIKit
+
+class LocaisViagemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Minhas-Viagens-App
